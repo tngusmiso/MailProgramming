c0161309edc2e839bef3e6f188027e292ad0b7ce
tngusmiso <k1miso012@gmail.com>
2020-05-04 17:28:19 +0900

    [190617] map에 value값으로 c2가 포함되어있는지 조건 추가

--- 190617/java190617.java
+++ 190617/java190617.java
@@ -2,7 +2,6 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class java190617 {
-    
     public static void main(String[] args) {
 
         System.out.println(solution("EGG", "FOO"));
@@ -12,15 +11,17 @@ public class java190617 {
 
     public static boolean solution(String str1, String str2) {
         Map<Character, Character> keyMap = new HashMap<>();
-        Character c1, c2;
 
         for(int i=0; i<str1.length(); ++i){
-            c1 = str1.charAt(i);
-            c2 = str2.charAt(i);
-            if(keyMap.containsKey(c1))
-                if(keyMap.get(c1) != c2)
-                    return false;
+            char c1 = str1.charAt(i);
+            char c2 = str2.charAt(i);
 
+            if(keyMap.containsKey(c1) && keyMap.get(c1) != c2)
+                return false;
+
+            else if(keyMap.containsValue(c2))
+                return false;
+            
             keyMap.put(c1,c2);
         }
         return true;
