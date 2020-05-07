1f51eceaacfc0a6426e502d7ee2de5550e1e044a
tngusmiso <k1miso012@gmail.com>
2020-05-07 22:22:59 +0900

    [190701] N번째로 큰 배열 원소를 찾으시오.

---
+++ 190701/java190701.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class java190701{
+    public static void main(String[] args) {
+
+        System.out.println(solution(new int[]{-1, 3, -1, 5, 4}, 2));
+        System.out.println(solution(new int[]{2, 4, -2, -3, 8}, 1));
+        System.out.println(solution(new int[]{-5, -3, 1}, 3));
+    }
+
+    public static int solution(int[] arr, int N) {
+        Arrays.sort(arr);
+        return arr[arr.length-N];
+    }
+}
