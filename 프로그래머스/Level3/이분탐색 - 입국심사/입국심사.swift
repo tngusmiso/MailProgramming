afefa723adc1daa6698553ec62f9abd50d182902
tngusmiso <k1miso012@gmail.com>
2020-10-19 17:17:14 +0900

    [프로그래머스] 이분탐색 - 입국심사 (Level3) Swift 코드 개선

--- 프로그래머스/Level3/이분탐색 - 입국심사/입국심사.swift
+++ 프로그래머스/Level3/이분탐색 - 입국심사/입국심사.swift
@@ -3,28 +3,27 @@ import Foundation
 func solution(_ n:Int, _ times:[Int]) -> Int64 {
     
     func binarySearch(start: Int64, end: Int64) -> Int64 {
+        if start > end { return -1 } // 잘못된 입력이 들어오면 -1 반환
+        
         let mid = (start+end)/2
         let midSum = available(in: mid)
         
-        // print("start:\(start), mid:\(mid), end: \(end)")
-        if midSum == n {
-            let leftSum = available(in: mid-1)
-            if leftSum < n {
+        // midSum >= n 일 경우
+        if midSum >= n {
+            if available(in: mid-1) < n {
                 return mid
             }
-            return binarySearch(start: start, end: mid-1)
-        }
-        if midSum < n {
-            let rightSum = available(in: mid+1)
-            if rightSum >= n {
-                return mid+1
-            }
-            return binarySearch(start: mid+1, end: end)
+            return binarySearch(start: 0, end: mid-1)
         }
         
-        return binarySearch(start: start, end: mid-1)
+        // midSum < n 일 경우
+        if available(in: mid+1) >= n {
+            return mid+1
+        }
+        return binarySearch(start: mid+1, end: end)        
     }
     
+    // minute분동안 입국심사를 완료할 수 있는 인원 수
     func available(in minute: Int64) -> Int64 {
         var sum: Int64 = 0
         for i in 0..<times.count {
@@ -33,5 +32,7 @@ func solution(_ n:Int, _ times:[Int]) -> Int64 {
         return sum
     }
     
-    return binarySearch(start: 0, end: Int64(n) * Int64(times.max()!))
+    let min = Int64(times.min()!)
+    let max = Int64(n) * Int64(times.max()!)
+    return binarySearch(start: min, end: max)
 }
