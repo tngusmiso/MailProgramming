9a84e583e92c511cb8e00cd52c15912abbd065f6
tngusmiso <k1miso012@gmail.com>
2021-01-10 04:44:59 +0900

    [프로그래머스] 탐욕법(Greedy) - 큰 수 만들기 (Level2) Swift 완전한 풀이

--- 프로그래머스/Level2/탐욕법(Greedy) - 큰 수 만들기/큰수만들기.swift
+++ 프로그래머스/Level2/탐욕법(Greedy) - 큰 수 만들기/큰수만들기.swift
@@ -28,32 +28,46 @@ func solution(_ number: String, _ k:Int) -> String {
     return answer
 }
 
-// Stack을 이용한 방법 (*수정 필요*)
+// Stack을 이용한 방법
 func solution2(_ number: String, _ k:Int) -> String {
     var numArr: [Character] = number.map { $0 }
     var stack: [Character] = []
-    var count: Int = 0
+    var delete: Int = 0
     var i: Int = 0
     
-    while count < k && i < numArr.count {
+    while delete < k && i < numArr.count {
+        // 스택이 비어있으면 
+        // 현재 인덱스의 숫자를 넣는다. (i 증가)
         guard let last = stack.last else {
             stack.append(numArr[i])
             i += 1
             continue
         }
+        
+        // 스택의 마지막 숫자보다 현재 인덱스의 숫자가 크면
+        // 스택의 마지막 숫자를 꺼내고 현재 인덱스의 숫자를 넣는다.
         if last < numArr[i] {
             stack.removeLast()
-            count += 1
+            delete += 1
             continue
         }
+        
+        // 스택에 현재 인덱스의 숫자를 넣는다.(i 증가)
         stack.append(numArr[i])
         i += 1
     }
     
-    if i < numArr.count {
+    // k개의 숫자를 제거 완료한 경우
+    // 아직 탐색 못한 숫자들까지 합친다.
+    if delete >= k {
         stack.append(contentsOf: numArr[i...])
     }
     
-    let answer = stack.reduce(""){"\($0)\($1)"}
-    return answer
+    // 인덱스를 끝까지 탐색했는데도 k개 제거하지 못했을 경우
+    // 뒤에서부터 k개는 제거한다
+    if i >= numArr.count {
+        return stack[0..<numArr.count-k].reduce(""){"\($0)\($1)"}
+    }
+    
+    return stack.reduce(""){"\($0)\($1)"}
 }
